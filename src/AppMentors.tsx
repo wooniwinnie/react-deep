376093c5b0140b04a057974b6e91f85cbf73db03
wooniwinnie <wooniwinnie@gmail.com>
2023-06-05 12:57:21 +0900

    수정함

--- src/AppMentors.tsx
+++ src/AppMentors.tsx
@@ -12,6 +12,40 @@ type Person = {
 };
 
 export default function AppMentors() {
+    const handleChange = () => {
+        const prev = prompt('누구를 바꾸고 싶니?');
+        const current = prompt('어떤 멘토로 바꾸고 싶니?');
+        // 1. 사용자가 멘토 이름 바꾸기 클릭하고 입력한 값(prev)이 현재 멘토 객체의 이름과 동일한지 비교해야함 : if문?
+        setPerson((person: Person) => ({
+            ...person,
+            mentors: person.mentors.map((mentor: Mentor) => {
+                if (mentor.name === prev) {
+                    // fallback value 추가
+                    // 사용자가 입력을 취소하면 null을 반환하기 때문에 타입오류 발생하는 것을 방지하기 위함!
+                    return { ...mentor, name: current || '' };
+                }
+                return mentor;
+            }),
+        }));
+    };
+    const handleAdd = () => {
+        const name = prompt('추가하고 싶은 멘토는?');
+        const title = prompt('멘토의 타이틀은?');
+        setPerson((person: Person) => ({
+            ...person,
+            mentors: [
+                ...person.mentors,
+                { name: name || '', title: title || '' },
+            ],
+        }));
+    };
+    const handleDelete = () => {
+        const name = prompt('삭제하고 싶은 멘토는?');
+        setPerson((person: Person) => ({
+            ...person,
+            mentors: person.mentors.filter((m: Mentor) => m.name !== name),
+        }));
+    };
     const [person, setPerson] = useState<Person>({
         name: '지운',
         title: '개발자',
@@ -39,54 +73,9 @@ export default function AppMentors() {
                     </li>
                 ))}
             </ul>
-            <button
-                onClick={() => {
-                    const prev = prompt('누구를 바꾸고 싶니?');
-                    const current = prompt('어떤 멘토로 바꾸고 싶니?');
-                    // 1. 사용자가 멘토 이름 바꾸기 클릭하고 입력한 값(prev)이 현재 멘토 객체의 이름과 동일한지 비교해야함 : if문?
-                    setPerson((person: Person) => ({
-                        ...person,
-                        mentors: person.mentors.map((mentor: Mentor) => {
-                            if (mentor.name === prev) {
-                                // fallback value 추가
-                                // 사용자가 입력을 취소하면 null을 반환하기 때문에 타입오류 발생하는 것을 방지하기 위함!
-                                return { ...mentor, name: current || '' };
-                            }
-                            return mentor;
-                        }),
-                    }));
-                }}
-            >
-                멘토 이름 바꾸기
-            </button>
-            <button
-                onClick={() => {
-                    const name = prompt('추가하고 싶은 멘토는?');
-                    const title = prompt('멘토의 타이틀은?');
-                    setPerson((person: Person) => ({
-                        ...person,
-                        mentors: [
-                            ...person.mentors,
-                            { name: name || '', title: title || '' },
-                        ],
-                    }));
-                }}
-            >
-                멘토 추가하기
-            </button>
-            <button
-                onClick={() => {
-                    const name = prompt('삭제하고 싶은 멘토는?');
-                    setPerson((person: Person) => ({
-                        ...person,
-                        mentors: person.mentors.filter(
-                            (m: Mentor) => m.name !== name
-                        ),
-                    }));
-                }}
-            >
-                멘토 삭제하기
-            </button>
+            <button onClick={handleChange}>멘토 이름 바꾸기</button>
+            <button onClick={handleAdd}>멘토 추가하기</button>
+            <button onClick={handleDelete}>멘토 삭제하기</button>
         </div>
     );
 }
