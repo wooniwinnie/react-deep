4ef9b46d6c99eaadb5b8649fb084c9839919ebcc
wooniwinnie <wooniwinnie@gmail.com>
2023-06-19 13:17:54 +0900

    fix

--- src/AppMentors.tsx
+++ src/AppMentors.tsx
@@ -1,43 +1,67 @@
-import React, { useReducer } from 'react';
-import personReducer from './reducer/personReducer';
-
-type Mentor = {
-    name: string;
-    title: string;
-};
+import React, { useState } from 'react';
 
 type Person = {
     name: string;
     title: string;
-    mentors: Mentor[];
+    mentors: {
+        name: string;
+        title: string;
+    }[];
 };
 
-export default function AppMentors() {
-    const [person, dispatch] = useReducer(personReducer, initialPerson);
-    // const [person, setPerson] = useState<Person>();
-    const handleChange = () => {
-        const prev = prompt('누구를 바꾸고 싶니?');
-        const current = prompt('어떤 멘토로 바꾸고 싶니?');
-
-        dispatch({ type: 'CHANGE', prev, current });
+export const AppMentors = () => {
+    const [person, setPerson] = useState<Person>({
+        name: '지운',
+        title: '개발자',
+        mentors: [
+            {
+                name: '밥',
+                title: '시니어 개발자',
+            },
+            {
+                name: '제임스',
+                title: '시니어 개발자',
+            },
+        ],
+    });
+    const handleChangeMentor = () => {
+        const prev = prompt('누구를 바꿀까요?');
+        const cur = prompt('누구로 바꿀까요?');
+        setPerson((person) => ({
+            ...person,
+            mentors: person.mentors.map((mentor) => {
+                if (prev && cur && mentor.name === prev) {
+                    return { ...mentor, name: cur };
+                }
+                return mentor;
+            }),
+        }));
     };
-
-    const handleAdd = () => {
-        const name = prompt('추가하고 싶은 멘토는?');
-        const title = prompt('멘토의 타이틀은?');
-        dispatch({ type: 'ADD', name, title });
+    const handleAddMentor = () => {
+        const name = prompt('누구를 추가할까요?');
+        const title = prompt('직함이 무엇인가요?');
+        if (name && title) {
+            setPerson((person) => ({
+                ...person,
+                mentors: [...person.mentors, { name, title }],
+            }));
+        } else {
+            alert('멘토의 이름과 직함을 제대로 입력해주세요!');
+        }
     };
-    const handleDelete = () => {
-        const name = prompt('삭제하고 싶은 멘토는?');
-        dispatch({ type: 'DELETE', name });
+    const handleDeleteMentor = () => {
+        const name = prompt('누구를 삭제하실건가요?');
+        setPerson((person) => ({
+            ...person,
+            mentors: person.mentors.filter((mentor) => mentor.name !== name),
+        }));
     };
-
     return (
         <div>
             <h1>
                 {person.name}이는 {person.title}
             </h1>
-            <p>{person.name}의 멘토는 : </p>
+            <p>엘리의 멘토는 :</p>
             <ul>
                 {person.mentors.map((mentor, index) => (
                     <li key={index}>
@@ -45,24 +69,9 @@ export default function AppMentors() {
                     </li>
                 ))}
             </ul>
-            <button onClick={handleChange}>멘토 이름 바꾸기</button>
-            <button onClick={handleAdd}>멘토 추가하기</button>
-            <button onClick={handleDelete}>멘토 삭제하기</button>
+            <button onClick={handleChangeMentor}>멘토 교체</button>
+            <button onClick={handleAddMentor}>멘토 추가</button>
+            <button onClick={handleDeleteMentor}>멘토 삭제</button>
         </div>
     );
-}
-
-const initialPerson = {
-    name: '지운',
-    title: '개발자',
-    mentors: [
-        {
-            name: '밥',
-            title: '시니어개발자',
-        },
-        {
-            name: '제임스',
-            title: '시니어개발자',
-        },
-    ],
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
-import AppXY from './AppXY';
-import AppMentor from './AppMentor';
-import AppMentors from './AppMentors';
+import { AppMentors } from './AppMentors';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
